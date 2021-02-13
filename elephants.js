a5c2c276db88606afec910033a4843deb4aa5175
Sam Vader <samvader13@gmail.com>
2021-02-13 01:39:31 -0600

    better elephants

--- elephants.js
+++ elephants.js
@@ -27,9 +27,13 @@ class Elephant {
   }
 
   checkCollision(other) {
-
+    // Get distances between the balls components
     let distanceVect = p5.Vector.sub(other.position, this.position);
+
+    // Calculate magnitude of the vector separating the balls
     let distanceVectMag = distanceVect.mag();
+
+    // Minimum distance before they are touching
     let minDistance = this.r + other.r;
 
     if (distanceVectMag < minDistance) {
@@ -39,15 +43,26 @@ class Elephant {
       other.position.add(correctionVector);
       this.position.sub(correctionVector);
 
-
+      // get angle of distanceVect
       let theta = distanceVect.heading();
+      // precalculate trig values
       let sine = sin(theta);
       let cosine = cos(theta);
+
+      /* bTemp will hold rotated ball this.positions. You 
+       just need to worry about bTemp[1] this.position*/
       let bTemp = [new p5.Vector(), new p5.Vector()];
 
+      /* this ball's this.position is relative to the other
+       so you can use the vector between them (bVect) as the 
+       reference point in the rotation expressions.
+       bTemp[0].this.position.x and bTemp[0].this.position.y will initialize
+       automatically to 0.0, which is what you want
+       since b[1] will rotate around b[0] */
       bTemp[1].x = cosine * distanceVect.x + sine * distanceVect.y;
       bTemp[1].y = cosine * distanceVect.y - sine * distanceVect.x;
 
+      // rotate Temporary velocities
       let vTemp = [new p5.Vector(), new p5.Vector()];
 
       vTemp[0].x = cosine * this.velocity.x + sine * this.velocity.y;
@@ -55,21 +70,31 @@ class Elephant {
       vTemp[1].x = cosine * other.velocity.x + sine * other.velocity.y;
       vTemp[1].y = cosine * other.velocity.y - sine * other.velocity.x;
 
+      /* Now that velocities are rotated, you can use 1D
+       conservation of momentum equations to calculate 
+       the final this.velocity along the x-axis. */
       let vFinal = [new p5.Vector(), new p5.Vector()];
 
+      // final rotated this.velocity for b[0]
       vFinal[0].x =
         ((this.m - other.m) * vTemp[0].x + 2 * other.m * vTemp[1].x) /
         (this.m + other.m);
       vFinal[0].y = vTemp[0].y;
 
+      // final rotated this.velocity for b[0]
       vFinal[1].x =
         ((other.m - this.m) * vTemp[1].x + 2 * this.m * vTemp[0].x) /
         (this.m + other.m);
       vFinal[1].y = vTemp[1].y;
 
+      // hack to avoid clumping
       bTemp[0].x += vFinal[0].x;
       bTemp[1].x += vFinal[1].x;
 
+      /* Rotate ball this.positions and velocities back
+       Reverse signs in trig expressions to rotate 
+       in the opposite direction */
+      // rotate balls
       let bFinal = [new p5.Vector(), new p5.Vector()];
 
       bFinal[0].x = cosine * bTemp[0].x - sine * bTemp[0].y;
@@ -77,12 +102,13 @@ class Elephant {
       bFinal[1].x = cosine * bTemp[1].x - sine * bTemp[1].y;
       bFinal[1].y = cosine * bTemp[1].y + sine * bTemp[1].x;
 
+      // update balls to screen this.position
       other.position.x = this.position.x + bFinal[1].x;
       other.position.y = this.position.y + bFinal[1].y;
 
       this.position.add(bFinal[0]);
 
-
+      // update velocities
       this.velocity.x = cosine * vFinal[0].x - sine * vFinal[0].y;
       this.velocity.y = cosine * vFinal[0].y + sine * vFinal[0].x;
       other.velocity.x = cosine * vFinal[1].x - sine * vFinal[1].y;
@@ -94,11 +120,12 @@ class Elephant {
     image(elephantImg, this.position.x, this.position.y, this.r * 2, this.r * 2);
   }
 }
-let elephants = [new Elephant(100, 400, 50), new Elephant(700, 500, 70), new Elephant(30, 300, 70), new Elephant(400, 320, 60), new Elephant(380, 430, 80), new Elephant(300, 300,100), new Elephant(300, 470, 90), new Elephant(350, 400, 30)];
+let elephants = [new Elephant(100, 400, 50), new Elephant(700, 500, 70), new Elephant(300, 500, 70), new Elephant(300, 300, 50), new Elephant(400, 500, 70), new Elephant(200, 500, 70), new Elephant(400, 200, 50), new Elephant(200, 500, 70), new Elephant(700, 500, 70)];
 function setup() {
   createCanvas(windowWidth - 5, windowHeight - 5);
   bgImg = loadImage('space.jpg');
   elephantImg = loadImage('elephant.png');
+  
   
 }
 
@@ -109,9 +136,9 @@ function draw() {
     b.update();
     b.display();
     b.checkBoundaryCollision();
-    elephants[0].checkCollision(elephants[1]);
-    elephants[1].checkCollision(elephants[2]);
-    elephants[2].checkCollision(elephants[3]);
-    elephants[3].checkCollision(elephants[4]);
+  }
+  for (let i = 0; i < elephants.length - 1; i++) 
+  {
+    elephants[i].checkCollision(elephants[i + 1]);
   }
 }
